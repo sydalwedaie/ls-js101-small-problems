85fe69aeee5595c42079956cc9eaf2f2e9f5fcee
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-01-24 00:32:20 +0300

    Complete Easy 1 Ex04 Further Exploration.

--- easy-1/ex04.js
+++ easy-1/ex04.js
@@ -1,18 +1,47 @@
 // Build a program that asks the user to enter the length and width of a room in meters, and then logs the area of the room to the console in both square meters and square feet.
 
+// Further Exploration
+// Modify the program so that it asks the user for the input type (meters or feet). Compute for the area accordingly, and log it and its conversion in parentheses.
+
 let rlSync = require("readline-sync");
+const SQMETERS_TO_SQFEET = 10.7639;
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
 prompt("Welcom to Area Calculator!");
-prompt("Enter the length of the room in meters:");
+
+prompt("Enter the desired input type: 1) Meters 2) Feet");
+let inputChoice = rlSync.question();
+
+switch (inputChoice) {
+  case "1":
+    inputChoice = "meters";
+    break;
+  case "2":
+    inputChoice = "feet";
+    break;
+}
+
+prompt(`Enter the length of the room in ${inputChoice}:`);
 let length = rlSync.question();
 
-prompt("Enter the width of the room in meters:");
+prompt(`Enter the width of the room in ${inputChoice}:`);
 let width = rlSync.question();
 
-let areaSqmt = length * width;
+let area = length * width;
 
-prompt(`The area of the room is ${areaSqmt} meters (${areaSqmt * 10.7639})`);
+if (inputChoice === "meters") {
+  prompt(
+    `The area of the room is ${area} square meters (${(
+      area * SQMETERS_TO_SQFEET
+    ).toFixed(2)} feet).`
+  );
+} else if (inputChoice === "feet") {
+  prompt(
+    `The area of the room is ${area} square feet (${(
+      area / SQMETERS_TO_SQFEET
+    ).toFixed(2)} meters).`
+  );
+}
