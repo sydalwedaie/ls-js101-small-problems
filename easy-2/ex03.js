f73e038052cbc99cdc801444ac2031142da3e409
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-02-05 06:54:09 +0300

    Complete Easy 2 Ex03

---
+++ easy-2/ex03.js
@@ -0,0 +1,11 @@
+// Multiplying Two Numbers
+
+/*
+Create a function that takes two arguments, multiplies them together, and returns the result.
+*/
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiply(5, 3) === 15); // logs true
