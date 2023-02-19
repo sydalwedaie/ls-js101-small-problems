e59776c4f019b95fa1148eeb80fe6e6708b4965d
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-02-20 01:40:57 +0700

    Complete Easy 2 Ex12

---
+++ easy-2/ex12.js
@@ -0,0 +1,9 @@
+// Always Return Negative
+
+function negative(number) {
+  return -Math.abs(number);
+}
+
+console.log(negative(5)); // -5
+console.log(negative(-3)); // -3
+console.log(negative(0)); // -0
